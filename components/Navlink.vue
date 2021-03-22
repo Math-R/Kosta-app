dda8d3b559aa4877a59cb804ec99772f533c42b8
Mathieu Richard <mathieu.richard1@outlook.fr>
2021-03-22 23:21:52 +0100

    sass loading and resolve webpack bug + navbar

---
+++ components/Navlink.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="nav-link">
+    <nuxt-link :to="link" >
+      <div class="relative">
+        <div class="icon absolute">
+          <svg xmlns="http://www.w3.org/2000/svg" width="80" height="10.881" viewBox="0 0 104.491 10.881">
+            <path id="Tracé_4" data-name="Tracé 4" d="M5568.533,0s-.5,9.029,1.959,9.029c11.293,1.242,37.678,1.927,49.567,1.846,9.221-.063,40.066-.641,51.4-1.846,2.165-.274,1.392-9.029,1.392-9.029Z" transform="translate(-5568.491)" fill="#c85353"/>
+          </svg>
+        </div>
+        <div class="label">{{ text }}</div>
+      </div>
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navlink",
+  props: {
+    text: {
+      type: String,
+      required: true,
+      default: 'Button'
+    },
+    link: {
+      type: String,
+      required: true,
+      default: '/'
+    }
+  },
+  computed: {
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.nav-link {
+  min-width: 100px;
+  @apply flex items-center justify-center  px-6 leading-5 font-bold text-base transition-all duration-300;
+  .icon {
+    left: 50%;
+    top: -16px;
+    opacity: 0;
+    transition: .2s ease-in-out all;
+    transform : translateX(-50%);
+  }
+  .nuxt-link-active {
+    .icon {
+      opacity: 1;
+    }
+  }
+  &:hover {
+    transition: .2s ease-in-out all;
+    .icon {
+      opacity: 1;
+    }
+  }
+}
+</style>
---
+++ components/layout/Navbar.vue
@@ -0,0 +1,69 @@
+<template>
+  <section
+    :class="['navbar w-full py-3 px-6 md:px-12 transition-all duration-300 relative']">
+    <div class="w-full flex items-center justify-between">
+      <nuxt-link to="/" title="Kosta">
+        <img width="150" src="" alt="kosta" class="hidden md:block">
+      </nuxt-link>
+      <div class="flex items-center">
+        <div class="hidden md:flex">
+          <Navlink  text="Albums" link="/albums"/>
+          <Navlink  text="Profile" link="/profile"/>
+        </div>
+        <div class="md:hidden" @click="toggleMenu">
+          <!-- Menu mobile -->
+        </div>
+      </div>
+    </div>
+<!--    <div class="sticky flex flex-col  items-center mobile__submenu w-full" :class="subMenuOpen ? 'open py-4': ''">-->
+<!--      <nuxt-link class="mb-4" to="/albums">Albums</nuxt-link>-->
+<!--      <nuxt-link class="" to="/profile">Profile</nuxt-link>-->
+<!--    </div>-->
+  </section>
+</template>
+<script>
+import Navlink from "~/components/Navlink";
+export default {
+  components: {Navlink},
+  mixins: [],
+  computed: {
+    theme() {
+    },
+  },
+  data(){
+    return {
+      subMenuOpen : false,
+    }
+  },
+  mounted() {
+
+  },
+  methods: {
+    toggleMenu(){
+      this.subMenuOpen = !this.subMenuOpen
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+  @apply transition-all duration-300;
+  &.has-scrolled {
+    -webkit-box-shadow: 0px 12px 20px 0px rgba(0, 0, 0, 0.04);
+    -moz-box-shadow: 0px 12px 20px 0px rgba(0, 0, 0, 0.04);
+    box-shadow: 0px 12px 20px 0px rgba(0, 0, 0, 0.04);
+  }
+}
+.mobile__submenu{
+  bottom:0;
+  left: 0;
+  height: 0px;
+  transition: all 0.3s;
+  overflow: hidden;
+  &.open {
+    height: auto;
+    transition: all 0.3s;
+  }
+}
+</style>
