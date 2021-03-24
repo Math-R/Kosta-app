eff1713cb21686b1e4ca230142362806658bcd08
Mathieu Richard <mathieu.richard1@outlook.fr>
2021-03-24 22:33:18 +0100

    👌 IMPROVE: Auth view

--- components/auth/login.vue
+++ components/auth/login.vue
@@ -1,18 +1,13 @@
 <template>
-    <div class="Login">
-        <div class="Login__Container">
-            <img src="https://images.unsplash.com/photo-1518998053901-5348d3961a04?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80" alt="Cover Kosta"/>
-            <form>
-                <img src="~/assets/images/logo.png" alt="Logo Kosta"/>
-                <h2>S'identifier</h2>
-                <input type="text" alt="" />
-                <input type="password" alt=""/>
-                <button>Se connecter</button>
-                <span>Mot de passe oublié ?</span>
-                <span>Pas de compte ? Inscrivez-vous</span>
-            </form>
-        </div>
-    </div>
+      <form>
+        <img src="~/assets/images/logo.png" alt="Logo Kosta"/>
+        <h2 class="font-bold text-3xl">S'identifier</h2>
+        <input type="text" alt="" />
+        <input type="password" alt=""/>
+        <button>Se connecter</button>
+        <span>Mot de passe oublié ?</span>
+        <span>Pas de compte ? Inscrivez-vous</span>
+      </form>
 </template>
 
 <script>
@@ -23,7 +18,6 @@ export default {
 
 <style>
     .Login {
-        height: 100vh;
         width: 100vw;
         overflow: hidden;
         display: flex;
@@ -36,14 +30,13 @@ export default {
         width: 90vw;
         display: flex;
         box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.16);
-        border-radius: 50px;
         overflow: hidden;
     }
 
     .Login__Container > img {
         width: 50%;
     }
-    
+
     .Login__Container > form {
         width: 50%;
         display: flex;
@@ -53,8 +46,7 @@ export default {
     }
 
     .Login__Container > form > h2 {
-        font-size: 2rem;
-        font-weight: bold;
+
         color: #404040;
         font-family: 'Poppins';
         width: 60%;
---
+++ pages/auth.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="Login h-screen">
+    <div class="Login__Container rounded-xl">
+      <img src="https://images.unsplash.com/photo-1518998053901-5348d3961a04?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80" alt="Cover Kosta"/>
+      <Login v-if="!$auth.loggedIn"/>
+      <Register v-else/>
+    </div>
+  </div>
+</template>
+
+<script>
+import login from "@/components/auth/login";
+export default {
+  name:"auth",
+  components:[login]
+}
+</script>
