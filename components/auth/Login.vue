cea6d39276a64f4f44f4607df9caf97193e2b526
Thoumar <thomas@thoumar.com>
2021-03-24 23:28:52 +0100

    Adding images and refactoring CSS for Login and Register

--- components/auth/Login.vue
+++ components/auth/Login.vue
@@ -1,79 +1,17 @@
 <template>
-      <form>
-        <img src="~/assets/images/logo.png" alt="Logo Kosta"/>
+    <form>
+        <img class="Logo" src="~/assets/images/logo.png" alt="Logo Kosta"/>
         <h2 class="font-bold text-3xl">S'identifier</h2>
-        <input type="text" alt="" />
-        <input type="password" alt=""/>
+        <input placeholder="E-mail" type="text" alt="" />
+        <input placeholder="Mot de passe" type="password" alt=""/>
         <button>Se connecter</button>
         <span>Mot de passe oublié ?</span>
         <span>Pas de compte ? Inscrivez-vous</span>
-      </form>
+    </form>
 </template>
 
 <script>
 export default {
-  name:"Login"
+    name:"Login"
 }
 </script>
-
-<style>
-    .Login {
-        width: 100vw;
-        overflow: hidden;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-
-    .Login__Container {
-        height: 90vh;
-        width: 90vw;
-        display: flex;
-        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.16);
-        overflow: hidden;
-    }
-
-    .Login__Container > img {
-        width: 50%;
-    }
-
-    .Login__Container > form {
-        width: 50%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        flex-direction: column;
-    }
-
-    .Login__Container > form > h2 {
-
-        color: #404040;
-        font-family: 'Poppins';
-        width: 60%;
-        margin-bottom: 1rem;
-    }
-
-    .Login__Container > form > input {
-        height: 50px;
-        width: 60%;
-        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.16);
-        border-radius: 15px;
-        margin-bottom: 1rem;
-    }
-
-    .Login__Container > form > button {
-        width: 60%;
-        background: #DE0000;
-        color: white;
-        border-radius: 15px;
-        font-size: 17px;
-        height: 50px;
-        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.16);
-        margin-bottom: 1rem;
-    }
-
-    .Login__Container > form > span {
-        width: 60%;
-        margin-bottom: 1rem;
-    }
-</style>
--- pages/auth.vue
+++ pages/auth.vue
@@ -1,18 +1,95 @@
 <template>
-  <div class="Login h-screen">
-    <div class="Login__Container rounded-xl">
-      <img src="https://images.unsplash.com/photo-1518998053901-5348d3961a04?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80" alt="Cover Kosta"/>
-      <Login v-if="!$auth.loggedIn"/>
+  <div class="Auth h-screen">
+    <div class="Auth__Container rounded-xl">
+      
+      <img v-if="false" src="~/assets/images/background-clouds.png" alt="Cover Kosta"/>
+      <img v-else src="~/assets/images/background-museum.png" alt="Cover Kosta"/>
+      
+      <Login v-if="false"/>
       <Register v-else/>
     </div>
   </div>
 </template>
 
 <script>
-import Login from "@/components/auth/Login";
-import Register from "@/components/auth/Register"
-export default {
-  name:"auth",
-  components:{Login, Register}
-}
+  import Login from "@/components/auth/Login";
+  import Register from "@/components/auth/Register"
+  export default {
+    name:"auth",
+    components:{Login, Register}
+  }
 </script>
+
+<style>
+    .Auth {
+        width: 100vw;
+        overflow: hidden;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background: url("~assets/images/bubbles-one-background.png");
+        background-size: 95%;
+        background-repeat: no-repeat;
+        background-position: center;
+    }
+
+    .Auth__Container {
+        width: 80%;
+        height: 45rem;
+        display: flex;
+        overflow: hidden;
+        max-width: 75rem;
+        background: white;
+        box-shadow: 0px 0px 10px rgb(0, 0, 0, 0.16);
+    }
+
+    .Auth__Container > img {
+        width: 50%;
+        height: 100%;
+    }
+
+    .Auth__Container > form {
+        width: 50%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+    }
+
+    .Auth__Container > form > img {
+        margin-bottom: 3rem;
+    }
+
+    .Auth__Container > form > h2 {
+
+        color: #404040;
+        font-family: 'Poppins';
+        width: 60%;
+        margin-bottom: 1rem;
+    }
+
+    .Auth__Container > form > input {
+        padding-left: 1rem;
+        height: 50px;
+        width: 60%;
+        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.16);
+        border-radius: 15px;
+        margin-bottom: 1rem;
+    }
+
+    .Auth__Container > form > button {
+        width: 60%;
+        background: #DE0000;
+        color: white;
+        border-radius: 15px;
+        font-size: 17px;
+        height: 50px;
+        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.16);
+        margin-bottom: 1rem;
+    }
+
+    .Auth__Container > form > span {
+        width: 60%;
+        margin-bottom: 1rem;
+    }
+</style>
