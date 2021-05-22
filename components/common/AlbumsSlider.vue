db5372e1ef9f1a6bf7ac77c0edb9524b1b6e16df
Mathilde <mathilde.jolly@kokoroe.fr>
2021-05-22 17:15:29 +0200

    Add dropzone (not fonctionnal), add fontawesome, add album display choice (slider or grid)

---
+++ components/common/AlbumsSlider.vue
@@ -0,0 +1,103 @@
+<template>
+  <div>
+    <swiper v-if="display == 'slider'" class="swiper" :options="swiperOption">
+      <Album
+        v-for="album in albums"
+        :key="album.id"
+        v-bind:text="album.name"
+        :size="lg"
+        :isSwiper="true"
+      ></Album>
+      <router-link
+        to="albums/create"
+        tag="div"
+        class="swiper-slide px-2 w-full "
+      >
+        <div class="createAlbum"><h2>Créer un album</h2></div>
+      </router-link>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+import Album from "@/components/common/Album";
+
+import { Swiper, SwiperSlide } from "vue-awesome-swiper";
+import "swiper/css/swiper.css";
+
+export default {
+  name: "albumsSlider",
+  props: {
+    albums: {
+      type: Array,
+      default: "",
+    },
+  },
+  components: {
+    Album,
+    Swiper,
+    SwiperSlide,
+  },
+  middleware: "auth",
+  data() {
+    return {
+      display: "slider",
+      swiperOption: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+        grabCursor: true,
+        // loop: true,
+        // freeMode: true,
+        keyboard: {
+          enabled: true,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+          dynamicBullets: true,
+        },
+        breakpoints: {
+          1280: {
+            slidesPerView: 4,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 10,
+          },
+          768: {
+            slidesPerView: 2,
+          },
+          320: {
+            slidesPerView: 1,
+          },
+        },
+      },
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.createAlbum {
+  @apply flex justify-center items-center rounded-2xl;
+  border: 2px rgb(200, 200, 200) dashed;
+  height: 400px;
+  transition: transform 0.2s;
+
+  h2 {
+    @apply font-bold;
+    color: rgb(200, 200, 200);
+  }
+
+  &:hover {
+    border-color: #c85353;
+    transform: scale(0.97);
+
+    h2 {
+      color: #c85353;
+    }
+  }
+}
+</style>
+
--- pages/albums/create.vue
+++ pages/albums/create.vue
@@ -3,15 +3,45 @@
     <h1 class="title">
       Créer votre album
     </h1>
+    <dropzone id="dropzone" ref="el"
+      :options="options"
+      @vdropzone-file-added="addPhotos"
+      :destroyDropzone="true">
+    </dropzone>
+    <Button @click.native="addPhotos">Remove All Files</Button>
   </div>
 </template>
 
 <script>
+import Button from "@/components/common/Button";
+import Dropzone from 'nuxt-dropzone'
+import 'nuxt-dropzone/dropzone.css'
 
 export default {
   name: "create",
+  components: {
+    Dropzone,
+    Button
+  },
   middleware: 'auth',
   data() {
+    return {
+      // See https://rowanwins.github.io/vue-dropzone/docs/dist/index.html#/props
+      options: {
+        url: "https://httpbin.org/post",
+        headers: { "Authorization": "Bearer " + localStorage.getItem('token') }
+      }
+    }
+  },
+  methods: {
+    async addPhotos(file) {
+      console.log(file.upload)
+      await this.$axios.post('http://localhost:8000/api/album',
+        {name: 'testfront', photos : [file.upload]}
+      ).then(response => {
+        console.log(response);
+      });
+    }
   }
 }
 </script>
