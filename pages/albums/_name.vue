cb6c8a9adf203190ccf59baf4df04af75563d6bc
Mathieu Richard <mathieu.richard1@outlook.fr>
2021-06-11 00:29:39 +0200

    🐛 FIX: masonry

--- pages/albums/_name.vue
+++ pages/albums/_name.vue
@@ -1,15 +1,14 @@
 <template>
-  <section class="album">
-    {{album.title}}
-    <div class="">
-      <VueMasonryWall :items="album.days[0].photos" :options="{width: 300, padding: 12}">
-        <template v-slot:default="{item}">
-          <img :src="item.url" alt="">
-          <div class="item">
+  <section class="album py-10">
+    <div class="container">
+      <div class="text-7xl text-center font-bold mb-10">
+        {{ album.title }}
+      </div>
+      <div class="masonry mx-auto ">
+        <div class="bg-center bg-no-repeat bg-cover rounded-xl" v-for="(photo, index) in album.days[0].photos"
+             :style="{backgroundImage: `url(${photo.url})`}" :key="index"></div>
+      </div>
 
-          </div>
-        </template>
-      </VueMasonryWall>
     </div>
   </section>
 </template>
@@ -18,12 +17,14 @@
 import VueMasonryWall from "vue-masonry-wall";
 
 export default {
-  components:{VueMasonryWall},
+  components: {VueMasonryWall},
   data() {
     return {
       album: {
         title: 'test',
         description: '',
+        start_date:'11-06-2011',
+        end_date:'14-06-2011',
         days: [
           {
             date: '11-06-2021',
@@ -54,7 +55,19 @@ export default {
               },
               {
                 date: '11-06-2021',
-                url: require('~/assets/images/7.jpg')
+                url: require('~/assets/images/8.jpg')
+              },
+              {
+                date: '11-06-2021',
+                url: require('~/assets/images/9.jpg')
+              },
+              {
+                date: '11-06-2021',
+                url: require('~/assets/images/10.jpg')
+              },
+              {
+                date: '11-06-2021',
+                url: require('~/assets/images/6.jpg')
               },
               {
                 date: '11-06-2021',
@@ -71,7 +84,7 @@ export default {
             ]
           },
           {
-            date: '11-07-2021',
+            date: '12-06-2021',
             photos: [
               {
                 date: '11-06-2021',
@@ -99,10 +112,6 @@ export default {
               },
               {
                 date: '11-06-2021',
-                url: require('~/assets/images/7.jpg')
-              },
-              {
-                date: '11-06-2021',
                 url: require('~/assets/images/8.jpg')
               },
               {
@@ -119,17 +128,34 @@ export default {
       }
     }
   },
-  methods:{
-    append() {
-      for (let i = 0; i < 20; i++) {
-        const id = Math.floor(Math.random() * 1000)
-        const height = Math.floor(Math.random() * 200) + 600
-        this.items.push({
-          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas non sagittis leo. Vestibulum sit amet metus nec neque dignissim dapibus.',
-          image: `https://picsum.photos/id/${id}/600/${height}`
-        })
-      }
+  methods: {
+    width() {
+      return Math.floor(Math.random() * 4) + 1
     }
   }
 }
 </script>
+<style lang="scss">
+.masonry {
+  display: grid;
+  gap: 3em;
+  grid-template-columns: repeat(auto-fit, minmax(20em, 1fr));
+  grid-auto-rows: minmax(15em, auto);
+  grid-auto-flow: dense;
+
+
+  div:nth-child(4n) {
+    grid-column: span 2;
+    grid-row: span 2;
+  }
+
+  div:nth-child(7n) {
+    grid-column: span 2;
+  }
+
+  div:nth-child(3n) {
+    grid-row: span 2;
+  }
+
+}
+</style>
