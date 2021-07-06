8a5ebcd1ad6438370523197d5a29dbc77877c61f
moostack <contact@moostack.fr>
2021-07-06 23:33:15 +0200

    Auto stash before merge of "main" and "origin/main"

--- components/albumCreation/CollaboratorsStep.vue
+++ components/albumCreation/CollaboratorsStep.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="w-3/4 m-auto">
-    <input placeholder="Email" type="text" v-model="collaborators" />
+
+    <div class="flex items-center" v-for="(collaborateur,index) in collab">
+      <input placeholder="Email" type="email" v-model="collab[index].email" />
+      <Button
+        text="+"
+        class="customBtnPlus"
+        @click.native="addCollba"
+        v-if="index === 0"
+      ></Button>
+    </div>
 
     <Button
       text="Valider"
@@ -16,16 +25,29 @@ export default {
     return {
       stepTitle: "Invitez des ami.e.s",
       collaborators: "",
+      collab : [
+        {email: ''}
+      ],
     };
   },
   mounted() {
     this.$emit("set-title", this.stepTitle);
   },
   methods: {
+    async addCollba(){
+      this.collab.push({email: ''});
+      // const { data } = await this.$axios.post(
+      //   "http://localhost:8000/api/album/" + this.albumId + "/collaborator",
+      //   {email : this.collaborators}
+      // );
+      //
+      // // this.$emit("set-album-id", data.data.id);
+      // this.$emit("next-step", data);
+    },
     async submit() {
       const { data } = await this.$axios.post(
         "http://localhost:8000/api/album/" + this.albumSlug + "/invite",
-        { email : this.collaborators }
+        { email : this.collab }
       );
 
       this.$emit("next-step", data);
@@ -39,5 +61,9 @@ input {
   @apply w-full mb-4 pl-4 rounded-xl;
   height: 50px;
   box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.16);
+}
+.customBtnPlus{
+  margin-left: 10px;
+  margin-bottom: 1rem !important;
 }
 </style>
